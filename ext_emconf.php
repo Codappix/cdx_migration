9c956afb896fa62cf0aa7ad55adf97ce6683eaef
Daniel Siepmann <coding@daniel-siepmann.de>
2017-07-01 16:36:51 +0200

    TASK: Add TYPO3 necessary files
    Also add first controller.

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TYPO3 Content migration',
+    'description' => 'Allows developers to build migrations from one table or database to another.',
+    'category' => 'misc',
+    'version' => '0.0.1',
+    'state' => 'alpha',
+    'author' => 'Codappix',
+    'author_email' => 'info@codappix.net',
+    'author_company' => 'Codappix',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.999',
+            'php' => '7.0.0-7.1.999',
+        ],
+    ],
+];
